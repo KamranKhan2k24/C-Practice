cdb95f0fa1d277d138850aa6af286f763d993845
KamranKhan2k24 <erkamranhere@gmail.com>
2026-08-16 15:46:18 +0530

    Added while loop program

---
+++ whileloop2.c
@@ -0,0 +1,13 @@
+// C program to demonstrate the use of while loop.
+#include <stdio.h>
+
+int main() {
+    int countdown = 3;
+
+    while (countdown > 0) {
+        printf("%d\n", countdown);
+        countdown--;
+    }
+    printf("Happy New Year!!\n");
+    return 0;
+}
