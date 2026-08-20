30d1bbca5b0ebfb51ad8061a0b8dff16c797edc3
KamranKhan2k24 <erkamranhere@gmail.com>
2026-08-20 21:41:49 +0530

    Added Garbage demo code

---
+++ garbage_demo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void display();
+void show();
+int main(){
+        int x=10;
+        printf("x=%d\n",x);
+        display();
+        show();
+        return 0;
+}
+void display(){
+        int a =40;
+        int x=15;
+        printf("a=%d\n",a);
+}
+void show(){
+        int b;
+        printf("b=%d\n",b);
+}
