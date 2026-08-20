f52677a8ded6d73468c5e27eb7721c8cd698c5c2
KamranKhan2k24 <erkamranhere@gmail.com>
2026-08-20 22:02:37 +0530

    4th garbage code

---
+++ garbage_demo4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void display();
+void show();
+int main(){
+        int x=10;
+        printf("x=%d\n",x);
+        display();
+        show();
+        return 0;
+}
+void display(){
+        int a =40;
+        printf("a=%d\n",a);
+}
+void show(){
+        int b;
+        printf("b=%d\n",b);
+}
