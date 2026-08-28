4fe9f121018d36a56fddfa17ec6c77039db79d3d
KamranKhan2k24 <erkamranhere@gmail.com>
2026-08-28 23:24:19 +0530

    dma2

---
+++ dma2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+        int x=10,i;
+        int* p=(int*)malloc (10*sizeof(int));
+        for(i=0;i<10;i++){
+                p[i]=i; /**(p+i) */
+        }
+        for(i=0;i<10;i++){
+                printf("%d\t",p[i]);
+        }
+        printf("\n");
+        return 0;
+}
