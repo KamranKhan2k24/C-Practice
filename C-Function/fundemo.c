35008aaee8b337f3176ef12e772497401094faf8
KamranKhan2k24 <erkamranhere@gmail.com>
2026-08-31 22:25:09 +0530

    Fun demo

---
+++ C-Function/fundemo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void calculateSum();
+
+int main() {
+  calculateSum();
+  return 0;
+}
+
+void calculateSum() {
+  int x = 5;
+  int y = 10;
+  int sum = x + y;
+  printf("The sum of x + y is: %d", sum);
+}
+
