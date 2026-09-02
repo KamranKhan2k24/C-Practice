00b161a3cfdcbb4c881382698df4dc6318d5225f
KamranKhan2k24 <erkamranhere@gmail.com>
2026-09-02 22:49:53 +0530

    String second program.

---
+++ String/string2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+        int main(){
+        char* name="Lucknow";
+        char city[]="Kanpur";
+        /* int len =-1;
+        while(name[++len]!='\0');*/
+        printf("length=%ld\n",strlen(name));
+        return 0;
+}
